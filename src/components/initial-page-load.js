8f3fcc74a7380d05649bb9cf294baec016be0a83
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-10-13 14:53:12 +0200

    Added two components for creating elements and appending dom elements

---
+++ src/components/initial-page-load.js
@@ -0,0 +1,15 @@
+import CreateElement from './create-element'
+
+// Functions purpose: Initially append page DOM elements
+export default function InitialPageLoad() {
+  // Grab content div from dist index.html file
+   const mainDiv = document.getElementById('content')
+  
+  // Create div element for left hand page column and append to mainDiv
+  const leftColumnDiv = CreateElement({element: 'div', className: 'leftColumn'})
+  mainDiv.appendChild(leftColumnDiv)
+
+  // Create div element for right hand page column and append to mainDiv
+  const rightColumnDiv = CreateElement({element: 'div', className: 'rightColumn'})
+  mainDiv.appendChild(rightColumnDiv)
+}
