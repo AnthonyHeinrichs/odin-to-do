c472e591f7521d6056640b1b41a01d0396d63f86
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-10-18 20:25:17 +0200

    Updated styling

--- src/components/layouts/filter-title-dom.js
+++ src/components/layouts/filter-title-dom.js
@@ -7,7 +7,7 @@ export default function FilterTitle() {
   const filterTitle = createElement({element: 'h2', className: 'title', domText: 'All tasks'})
   filterTitleDiv.appendChild(filterTitle)
   
-  const filterDescription = createElement({element: 'h3', className: 'description', domText: 'Here are all your tasks'})
+  const filterDescription = createElement({element: 'h3', className: 'description', domText: 'You can find all your tasks here'})
   filterTitleDiv.appendChild(filterDescription)
 
   return filterTitleDiv
